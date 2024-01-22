d4341a3a67538da19a0ef51202cba0989abef84b
commoncuckoo <bbukkuk002@gmail.com>
2024-01-22 15:27:53 +0900

    signup fucntion c

---
+++ innerPeace/src/main/java/com/social/innerPeace/dto/SignupDTO.java
@@ -0,0 +1,21 @@
+package com.social.innerPeace.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SignupDTO {
+    private String email;
+    private String password;
+    private String name;
+    private String phone;
+    private LocalDateTime birth;
+
+}
--- innerPeace/src/main/java/com/social/innerPeace/user/account/service/UserAccountService.java
+++ innerPeace/src/main/java/com/social/innerPeace/user/account/service/UserAccountService.java
@@ -1,4 +1,32 @@
 package com.social.innerPeace.user.account.service;
 
+import com.social.innerPeace.dto.SignupDTO;
+import com.social.innerPeace.entity.Healer;
+
 public interface UserAccountService {
+    Object findByEmail(String email);
+
+    String register(SignupDTO dto);
+
+    default Healer dtoToEntity(SignupDTO dto){
+        Healer entity = Healer.builder()
+                .healer_email(dto.getEmail())
+                .healer_name(dto.getName())
+                .healer_pw(dto.getPassword())
+                .healer_bitrh(dto.getBirth())
+                .healer_phone(dto.getPhone())
+                .build();
+        return entity;
+    }
+
+    default SignupDTO entityToDto(Healer entity){
+        SignupDTO dto = SignupDTO.builder()
+                .email(entity.getHealer_email())
+                .name(entity.getHealer_name())
+                .password(entity.getHealer_pw())
+                .birth(entity.getHealer_bitrh())
+                .phone(entity.getHealer_phone())
+                .build();
+        return dto;
+    }
 }
