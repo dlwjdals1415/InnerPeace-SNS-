5da9d0e3bfa57927ce76c1c8407fef3c294a592d
Benromach <dlwjdals6016@gmail.com>
2024-02-01 15:50:19 +0900

    postdetail front and postlist css update

--- innerPeace/src/main/java/com/social/innerPeace/dto/PosTListDTO.java
+++ innerPeace/src/main/java/com/social/innerPeace/dto/PosTListDTO.java
@@ -15,18 +15,5 @@ import java.util.List;
 @Builder
 public class PosTListDTO {
     private Long post_no;
-    private String post_title;
-    private String post_content;
-    private MultipartFile post_image;
     private String post_image_thumbnail;
-    private float map_point_lat;
-    private float map_point_lng;
-    private List<String> tags;
-    private String healer_id;
-    private String post_writer;
-    private String followstat;
-    private int likes;
-    private String likeposition;
-    private String profileImg;
-    private LocalDateTime post_regday;
 }
